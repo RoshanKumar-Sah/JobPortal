efc783398cafb06354a28bceb845b6de41635e09
Roshan Sah <mail4sahrs@gmail.com>
2023-06-23 20:29:31 +0545

    Mongo atlas connected

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/JobPortal')
+// mongoose.connect('mongodb://127.0.0.1:27017/JobPortal')
+//     .then(() => console.log('Connected!'));
+
+mongoose.connect('mongodb+srv://mail4sahrs:Rtyfghvbn%40123%24@jobportal.g7u8ge7.mongodb.net/JobPortal?retryWrites=true&w=majority')
     .then(() => console.log('Connected!'));
